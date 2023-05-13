5118f55bd67223b8b345ec7fc406cadd8ada5269
Timur <tiabumuaz@gmail.com>
2023-05-13 17:32:17 +0500

    Добавил кнопки

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from "react";
+
+import cn from "classnames";
+
 import { getStockQuotes } from "../../api/getStockQuotes";
 import { StocksType } from "../../types/stocks";
 import styles from "./App.module.scss";
+
+import arrowIcon from "../../assets/images/arrow-icon.svg";
 
 function App() {
   const [quotes, setQuotes] = useState<StocksType>();
@@ -30,41 +35,79 @@ function App() {
       <div className={styles.wrap}>
         <table className={styles.table}>
           <caption className={styles.title}>Акции авиакомпаний</caption>
-          <tr className={styles.header}>
-            <th>№</th>
-            <th>Название</th>
-            <th>Цена</th>
-          </tr>
-
-          {isLoading && (
-            <tr>
-              <td className={styles.message} colSpan={3}>
-                Загрузка...
-              </td>
+          <tbody>
+            <tr className={styles.header}>
+              <th>№</th>
+              <th>Название</th>
+              <th colSpan={2}>Цена</th>
             </tr>
-          )}
 
-          {isError && (
-            <tr>
-              <td className={styles.message} colSpan={3}>
-                Ошибка. Проверьте подключение к сети
-              </td>
-            </tr>
-          )}
-
-          {!!quotes &&
-            quotes?.slice(0, 10).map((item, i) => (
-              <tr key={item.companyName}>
-                <td>{i + 1}</td>
-                <td>{item.companyName}</td>
-                <td>
-                  {item.latestPrice ?? (
-                    <span className={styles.empty}>Нет данных</span>
-                  )}
+            {isLoading && (
+              <tr>
+                <td className={styles.message} colSpan={4}>
+                  Загрузка...
                 </td>
               </tr>
-            ))}
+            )}
+
+            {isError && (
+              <tr>
+                <td className={styles.message} colSpan={4}>
+                  Ошибка. Проверьте подключение к сети
+                </td>
+              </tr>
+            )}
+
+            {!!quotes &&
+              quotes
+                .slice(0, 10)
+                .map(({ companyName, latestPrice, ytdChange }, i) => (
+                  <tr key={companyName}>
+                    <td>{i + 1}</td>
+                    <td>{companyName}</td>
+
+                    {!!latestPrice ? (
+                      <>
+                        <td>${latestPrice}</td>
+                        <td>
+                          <span
+                            className={cn(styles.change, {
+                              [styles.inc]: ytdChange > 0,
+                              [styles.dec]: ytdChange < 0,
+                            })}
+                          >
+                            {ytdChange > 0 && "+"}
+                            {ytdChange.toFixed(4)}
+                          </span>
+                        </td>
+                      </>
+                    ) : (
+                      <>
+                        <td>
+                          <span className={styles.empty}>Нет данных</span>
+                        </td>
+                        <td>
+                          <span className={styles.empty}>Нет данных</span>
+                        </td>
+                      </>
+                    )}
+                  </tr>
+                ))}
+          </tbody>
         </table>
+
+        <button
+          className={cn(styles.button, styles.prev)}
+          aria-label="Previous button"
+        >
+          <img src={arrowIcon} alt="Previous arrow icon" />
+        </button>
+        <button
+          className={cn(styles.button, styles.next)}
+          aria-label="Next button"
+        >
+          <img src={arrowIcon} alt="Next arrow icon" />
+        </button>
       </div>
     </main>
   );
