213fe211f5e09f53d2d77df7418432feac63cd33
Timur <tiabumuaz@gmail.com>
2023-05-13 15:11:42 +0500

    Добавил ui

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,13 +5,17 @@ import styles from "./App.module.scss";
 
 function App() {
   const [quotes, setQuotes] = useState<StocksType>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
   const getData = async () => {
     try {
       const { data } = await getStockQuotes();
       setQuotes(data);
     } catch {
-      console.log("error");
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -23,22 +27,45 @@ function App() {
 
   return (
     <main className={styles.root}>
-      <h1 className={styles.title}>Акции авиакомпаний</h1>
-
-      <table>
-        <tr>
-          <td>№</td>
-          <td>Название</td>
-          <td>Цена</td>
-        </tr>
-        {quotes?.slice(0, 10).map((item, i) => (
-          <tr key={item.companyName}>
-            <td>{i + 1}</td>
-            <td>{item.companyName}</td>
-            <td>{item.latestPrice ?? '-'}</td>
+      <div className={styles.wrap}>
+        <table className={styles.table}>
+          <caption className={styles.title}>Акции авиакомпаний</caption>
+          <tr className={styles.header}>
+            <th>№</th>
+            <th>Название</th>
+            <th>Цена</th>
           </tr>
-        ))}
-      </table>
+
+          {isLoading && (
+            <tr>
+              <td className={styles.message} colSpan={3}>
+                Загрузка...
+              </td>
+            </tr>
+          )}
+
+          {isError && (
+            <tr>
+              <td className={styles.message} colSpan={3}>
+                Ошибка. Проверьте подключение к сети
+              </td>
+            </tr>
+          )}
+
+          {!!quotes &&
+            quotes?.slice(0, 10).map((item, i) => (
+              <tr key={item.companyName}>
+                <td>{i + 1}</td>
+                <td>{item.companyName}</td>
+                <td>
+                  {item.latestPrice ?? (
+                    <span className={styles.empty}>Нет данных</span>
+                  )}
+                </td>
+              </tr>
+            ))}
+        </table>
+      </div>
     </main>
   );
 }
